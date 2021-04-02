bdc9d50c52d97efb1a5ac41c632ceb2df18cf6c6
BBennettChiba <BrysonCBennett@gmail.com>
2021-04-02 10:28:56 +0900

    config still

--- ormconfig.js
+++ ormconfig.js
@@ -7,8 +7,10 @@ let config = {
    "username": process.env.DB_USERNAME,
    "password": process.env.DB_PASSWORD,
    "database": process.env.DB_DATABASE,
+   "url": process.env.DATABASE_URL,
    "synchronize": true,
    "logging": false,
+   ssl: {rejectUnauthorized:false},
    "entities": [
       "src/entity/**/*.ts"
    ],
@@ -26,11 +28,11 @@ let config = {
 }
 
 // const productionConn = null
-if (process.env.DATABASE_URL){
-   config = {}
-   config.type='postgres';
-   config.url = parse(process.env.DATABASE_URL);
-   config.ssl = {rejectUnauthorized:false};
-}
+// if (process.env.DATABASE_URL){
+//    config = {}
+//    config.type='postgres';
+//    config.url = parse(process.env.DATABASE_URL);
+//    config.ssl = {rejectUnauthorized:false};
+// }
 
 module.exports = config;
